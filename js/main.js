fbc2e1e147e76159e80eb6f67930f1c9351d914b
minho.Kim <minho0078@gmail.com>
2016-05-18 14:22:32 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -13,7 +13,6 @@ app.controller('setMenu', function($scope,$http){
 
    //SUB메뉴 선택 시, 메인 화면 HTML 호출.
    $scope.getMain = function(sub,$event){
-/*
          $http({
             method : "GET",
             url : "./html/java.html"
@@ -22,11 +21,12 @@ app.controller('setMenu', function($scope,$http){
          }, function myError(response){
             $scope.mainHtml = response.status.Text;
          });
-*/
+
+/*
          $http.get("./html/java.html")
          .then(function(resp){
             $scope.mainHtml = resp;
          });
    }
-
+*/
 });
